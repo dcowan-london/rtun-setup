9b7d105e7431966b6af89035cd17ea24d18dedae
Dovi Cowan <dovi@fullynetworking.co.uk>
2021-05-13 20:49:27 +0100

    added licensing

--- reverse-tunnel-client.sh
+++ reverse-tunnel-client.sh
@@ -9,6 +9,31 @@
 
 cd
 
+echo "REVERSE TUNNEL CLIENT SETUP"
+echo "One-stop script to install and set up https://github.com/snsinfu/reverse-tunnel"
+echo ""
+echo "Reverse Tunnel Copyright (c) 2018, 2021 snsinfu MIT License"
+echo "Setup script Copyright (c) 2021 Dovi Cowan MIT License"
+echo ""
+echo "Permission is hereby granted, free of charge, to any person obtaining a copy"
+echo "of this software and associated documentation files (the "Software"), to deal"
+echo "in the Software without restriction, including without limitation the rights"
+echo "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell"
+echo "copies of the Software, and to permit persons to whom the Software is"
+echo "furnished to do so, subject to the following conditions:"
+echo ""
+echo "The above copyright notice and this permission notice shall be included in all"
+echo "copies or substantial portions of the Software."
+echo ""
+echo "THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR"
+echo "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,"
+echo "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE"
+echo "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER"
+echo "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,"
+echo "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE"
+echo "SOFTWARE."
+echo
+
 apt update
 apt upgrade -y
 apt install git build-essential golang -y

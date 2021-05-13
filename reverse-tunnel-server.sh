96606bc7e51a8e7e30d9695285ed3ea1d74b600c
Dovi Cowan <dovi@fullynetworking.co.uk>
2021-05-13 15:37:21 +0100

    more fixing

--- reverse-tunnel-server.sh
+++ reverse-tunnel-server.sh
@@ -23,9 +23,9 @@ while [[ $IP_CORRECT -ne 1 ]]; do
     read -n 1 -p "Please confirm server IP address [$SERVER_IP] [y/n] "
     echo
 
-    if [[ ${REPLY} == 'n' ]]; then
+    if [[ ${REPLY} == "n" ]]; then
         read -p "Enter correct server IP: " SERVER_IP
-    elif [[ ${REPLY} == 'y' ]]; then
+    elif [[ ${REPLY} == "y" ]]; then
         IP_CORRECT=1
     else
         echo "You must enter y or n!"
@@ -43,7 +43,7 @@ ADD_AGENT=1
 while [[ $ADD_AGENT -eq 1 ]] do
 
     read -N 1 -p "Add client? [y/n] " CREATE && echo
-    if [[ $CREATE == 'y' ]] then
+    if [[ $CREATE == "y" ]] then
         KEY=$(openssl rand -hex 32)
         echo "Enter allowed ports for first client. Use format 'port/protocol', eg '80/tcp' (without the quotations). Separate ports with a comma and space, eg '80/tcp, 443/tcp'."
         read -p "Ports: " PORTS
@@ -57,7 +57,7 @@ EOF
         echo -e "\033[1mClient Key: $KEY\033[0m"
         echo
         
-    elif [[ $CREATE == 'n' ]] then
+    elif [[ $CREATE == "n" ]] then
         ADD_AGENT=0
     else
         echo "You must enter y or n!"
@@ -66,7 +66,7 @@ done;
 
 read -N 1 -p "Create service? [y/n] " SERVICE
 
-if [[ $SERVICE == 'y' ]] then
+if [[ $SERVICE == "y" ]] then
     tee /etc/systemd/system/rtun-server.service >/dev/null <<EOF
 [Unit]
 Description=RTUN
@@ -87,7 +87,7 @@ EOF
     read -N 1 -p "Start service now? [y/n]"
     echo
 
-    if [[ ${REPLY} == 'y' ]] then
+    if [[ ${REPLY} == "y" ]] then
         systemctl start rtun-server
         echo "Started server"
     fi
